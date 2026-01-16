7897aeac8a90fcc4908790598b9b14bfe3e23692
Calvin Chong <ccalvinoutlookcom@gmail.com>
2026-01-16 14:22:14 +0800

    FINALLY finished setting up the folders

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import dashboard.MainFrame;
+import java.util.ArrayList; // Import your frame from the dashboard folder
+import java.util.List;
+import javax.swing.SwingUtilities;
+import model.*;
+
+public class Main {
+   public static void main(String[] args) {
+
+       // 1. Create Mock Data
+       List<User> mockUsers = new ArrayList<>();
+       mockUsers.add(new Coordinator("C01", "admin", "admin123"));
+       mockUsers.add(new Student("S01", "ali", "123"));
+
+       // 2. Launch
+       SwingUtilities.invokeLater(() -> {
+           MainFrame frame = new MainFrame(mockUsers);
+           frame.setVisible(true);
+       });
+   }
+}
---
+++ src/model/Evaluator.java
@@ -0,0 +1,7 @@
+package model;
+public class Evaluator extends User{
+
+    public Evaluator(String id, String username, String password) {
+        super(id, username, password, "EVALUATOR");
+    }
+}
