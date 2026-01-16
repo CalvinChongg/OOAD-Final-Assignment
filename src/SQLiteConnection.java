657f4d60999936c17a40cbc4b7b945b8d4fd59c6
Calvin Chong <ccalvinoutlookcom@gmail.com>
2026-01-16 14:28:36 +0800

    database connection and git ignore

--- src/SQLiteConnection.java
+++ src/SQLiteConnection.java
@@ -4,5 +4,17 @@ import java.sql.SQLException;
 
 
 public class SQLiteConnection {
-    
+    public static void main(String[] args) {
+        String url = "jdbc:sqlite:database.db";
+
+        // write SQL code here
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                System.out.println("Connection to SQLite has been established.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
